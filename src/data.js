5a5bb60c80865a61dd82da2a1b767c66a27afba2
Pyae Sone Aung <pyaesoneag@isgm2.com>
2022-02-06 23:34:54 +0630

    add portfolio contets

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const projects = [
+  {
+    title: "iSGM Website",
+    subtitle: "Wordpress",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./isgmwebsite.png",
+    link: "https://isgm2.com",
+  },
+  {
+    title: "iHRMS & iRWMS",
+    subtitle: "Angular and SpringBoot",
+    description:
+      "iHRMS is easy to use and to reduce paperwork, speeds up a creation of documents, save times and reduce human resources about HR tasks.",
+    image: "./ihrms.png",
+    link: "https://ihrms.isgm.co.jp/",
+  },
+  {
+    title: "Building Automation",
+    subtitle: "SpringBoot (dummy image)",
+    description:
+      "Building automation that contributes to the efficiency of management and operation by monitoring and controlling various equipments.",
+    image: "./ba.jpg",
+    link: "#",
+  },
+  {
+    title: "AI Tower Searching",
+    subtitle: "TensorFlow & Django",
+    description:
+      "AI Tower Searching project is an easy and efficient way to find telecom tower using map, AI and web application",
+    image: "./ai.png",
+    link: "#",
+  },
+];
+
+export const skills = [
+  "SpringBoot",
+  "Angular",
+  "React",
+  "Wordpress",
+  "Code Igniter",
+  "Laravel",
+  "Python",
+  "C++",
+  "Android",
+  "Swift",
+];
+
+export const testimonials = [
+  {
+    id: "1",
+    quote:
+      "Working with PSA was better than expected and we had really high expectations. He is a talented developer but what really makes him stand out is his work ethic and steady approach.",
+    image: "./sanlinnaing.jpeg",
+    name: "San Lin Naing",
+    company: "General Manager",
+  },
+  {
+    id: "2",
+    quote:
+      'I\'m so proud of you. Boss also know your hard working and taking responsibility. I asked Boss "How do you know about PSA?" He answered me he know via NESIC HeadQuarters',
+    image: "./theinzaw.jpeg",
+    name: "Thein Zaw",
+    company: "Manager",
+  },
+];
+
+export const EMAIL_DATA = {
+  SERVICE_ID: `service_rhqnf8i`, // Service ID
+  USER_ID: `user_KjjMGBp7rDeCyBnOpcoOS`, //userID
+  TEMPLATE_ID: `template_contactme`, //templateID
+};
